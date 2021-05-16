d2ce54ab6071a0b16d244a12f426e9b2350816fb
Antonina Osipenko <emagcodeshake@gmail.com>
2021-05-16 16:53:52 +0300

    add build

---
+++ build/precache-manifest.d106b0e64301908c0f4b95d168989980.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e735e312a2ef03416b36f7e9612318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2674d243f9f2323967a4",
+    "url": "/static/js/10.ce4972b6.chunk.js"
+  },
+  {
+    "revision": "4a50339ba211b848fb55",
+    "url": "/static/js/2.4f261f7a.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/static/js/2.4f261f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b26e0eb52162a099a8",
+    "url": "/static/js/3.21079f0d.chunk.js"
+  },
+  {
+    "revision": "9eaeafe18004d248665c",
+    "url": "/static/js/4.2c7d0c26.chunk.js"
+  },
+  {
+    "revision": "09de97c19daf46c68060",
+    "url": "/static/js/5.c7afdd7a.chunk.js"
+  },
+  {
+    "revision": "0f951ab312d2efd1a198",
+    "url": "/static/js/6.8339adb0.chunk.js"
+  },
+  {
+    "revision": "cfd8733a19c4ddb3fcf5",
+    "url": "/static/js/7.02ebd48c.chunk.js"
+  },
+  {
+    "revision": "fa61d7a2e7df1d4b4408",
+    "url": "/static/js/8.7b45a303.chunk.js"
+  },
+  {
+    "revision": "fd46724be3bf466de87d",
+    "url": "/static/js/9.921e8494.chunk.js"
+  },
+  {
+    "revision": "1097b66269e6ef74fc9f",
+    "url": "/static/js/main.bb0bb0ca.chunk.js"
+  },
+  {
+    "revision": "905fb26baa3c25830e5e",
+    "url": "/static/js/runtime-main.49c760e7.js"
+  }
+]);
